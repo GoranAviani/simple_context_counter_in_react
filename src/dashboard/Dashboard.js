60188c0331af559372dc64e76756ac6e6e69fe1e
GoranAviani <goranaviani@gmail.com>
2022-05-14 11:28:42 +0200

    simple_context_counter_in_react

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Counter from "../counter/Counter";
+
 const Dashboard = () => {
     return(
         <React.Fragment>
             Dashboard
+            <Counter/>
         </React.Fragment>
     )
 }
