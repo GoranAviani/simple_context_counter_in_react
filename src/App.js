3e79302a5a5aaf67ca3c84344c2cb1fcbc6fcc2c
GoranAviani <goranaviani@gmail.com>
2022-05-14 11:54:22 +0200

    simple_context_counter_in_react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Dashboard from "./dashboard/Dashboard";
 
 import CounterContext from "./context/CounterContext";
+import {useState} from "react";
 
 function App() {
 
+    const [counterState, setCouterState] = useState(0)
+
 
   return (
-    <CounterContext.Provider value={{counter: 0}} className="App">
+    <CounterContext.Provider value={{counter: counterState, setCouterState: setCouterState}} className="App">
       <Dashboard/>
     </CounterContext.Provider>
   );
