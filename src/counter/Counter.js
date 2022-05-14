aee7e8043485f2b6af9e9b7ebcb6a940cf8a2b77
GoranAviani <goranaviani@gmail.com>
2022-05-14 12:02:50 +0200

    simple_context_counter_in_react

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -6,19 +6,11 @@ import CounterContext from "../context/CounterContext";
 const Counter = () => {
     const ctx = useContext(CounterContext)
 
-    const addHandler = () => {
-        ctx.setCouterState(ctx.counter + 1)
-    }
-
-    const removeHandler = () => {
-        ctx.setCouterState(ctx.counter - 1)
-
-    }
 
     return(
         <div>
-            <button onClick={addHandler}>+</button>
-            <button onClick={removeHandler}>-</button>
+            <button onClick={ctx.addHandler}>+</button>
+            <button onClick={ctx.removeHandler}>-</button>
 
         </div>
     )
